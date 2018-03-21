f275d660a63bf28b419e1002e24efef776fc5297
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2018-03-21 16:44:17 +0100

    add basic program e-paper

---
+++ epaper/epd.cpp
@@ -0,0 +1,115 @@
+#include "epd.h"
+#include "demo.c"
+
+Epd::Epd(SPI* spi, PinName cs_pin, PinName reset_pin, PinName dc_pin, PinName busy_pin):
+  cs(cs_pin), display_reset(reset_pin), dc(dc_pin), busy(busy_pin)
+{
+    this->spi = spi;
+    spi->format(8, 0);
+    spi->frequency(2000000);
+    width = 400;
+    height = 300;
+    init();
+}
+
+void Epd::init()
+{
+    reset();
+    sendCommand(BOOSTER_SOFT_START);
+    sendData(0x17);
+    sendData(0x17);
+    sendData(0x17);
+    sendCommand(POWER_ON);
+    waitUntilIdle();
+    sendCommand(PANEL_SETTING);
+    sendData(0x0F);
+}
+
+void Epd::sendCommand(unsigned char command)
+{
+    dc = 0;
+    cs = 0;
+    spi->write(command);
+    cs = 1;
+}
+
+void Epd::sendData(unsigned char data)
+{
+    dc = 1;
+    cs = 0;
+    spi->write(data);
+    cs = 1;
+}
+
+void Epd::waitUntilIdle()
+{
+    while(busy == 0){   // 0:busy, 1: idle
+        Thread::wait(100);
+    }
+}
+
+void Epd::reset()
+{
+    display_reset = 0;
+    Thread::wait(200);
+    display_reset = 1;
+    Thread::wait(200);
+}
+
+void Epd::clearFrame()
+{
+    sendCommand(DATA_START_TRANSMISSION_1);
+    Thread::wait(2);
+    for(int i = 0; i < width / 8 * height; i++) {
+        sendData(0xFF);
+    }
+    Thread::wait(2);
+    sendCommand(DATA_START_TRANSMISSION_2);
+    Thread::wait(2);
+    for(int i = 0; i < width / 8 * height; i++) {
+        sendData(0xFF);
+    }
+    Thread::wait(2);
+}
+
+/**
+ * @brief: After this command is transmitted, the chip would enter the deep-sleep mode to save power. 
+ *         The deep sleep mode would return to standby by hardware reset. The only one parameter is a 
+ *         check code, the command would be executed if check code = 0xA5. 
+ *         You can use Epd::Reset() to awaken and use Epd::Init() to initialize.
+ */
+void Epd::sleep()
+{
+    sendCommand(VCOM_AND_DATA_INTERVAL_SETTING);
+    sendData(0xF7);     // border floating
+    sendCommand(POWER_OFF);
+    waitUntilIdle();
+    sendCommand(DEEP_SLEEP);
+    sendData(0x15);
+}
+
+void Epd::displayFrame()
+{
+    sendCommand(DISPLAY_REFRESH);
+    Thread::wait(100);
+    waitUntilIdle();
+}
+
+void Epd::demo()
+{
+    sendCommand(DATA_START_TRANSMISSION_1);
+    Thread::wait(2);
+    for(int i = 0; i < this->width / 8 * this->height; i++) {
+        sendData(gImage_demo[i]);
+    }
+    Thread::wait(2);
+
+    sendCommand(DATA_START_TRANSMISSION_2);
+    Thread::wait(2);
+    for(int i = 0; i < this->width / 8 * this->height; i++) {
+        sendData(gImage_demo[i]);
+    }
+    Thread::wait(2);
+
+    displayFrame();
+}
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include "mbed.h"
+ 
+#include "epd.h"
+
+Serial pc(SERIAL_TX, SERIAL_RX);
+
+// DigitalOut myled(LED1);
+SPI spi(D11, D12, D13); // mosi, miso, sclk
+// DigitalOut cs(D0);
+
+Epd epd(&spi, D7, D6, D5, D4); // spi, cs, reset, dc, busy
+ 
+int main() {
+    pc.printf("\r\n** e-Paper demo **\r\n");
+
+    epd.reset();
+    epd.init();
+    epd.clearFrame();
+    epd.demo();
+
+    while(1) {
+        // myled = 1; // LED is ON
+        // wait(0.2); // 200 ms
+        // myled = 0; // LED is OFF
+        // wait(1.0); // 1 sec
+    }
+}
